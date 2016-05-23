c01df1b783a8d5f947b78cb8a0b1238294287b86
Jose Annunziato <jannunzi@gmail.com>
2016-05-23 15:55:14 -0400

    configured navigation with angular routing

---
+++ public/assignment/config.js
@@ -0,0 +1,15 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
+            });
+    }
+})();
