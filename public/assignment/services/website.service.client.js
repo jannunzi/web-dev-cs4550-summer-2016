68d74645e24a47f76f75b62ca4e8cac05627f0f9
Jose Annunziato <jannunzi@gmail.com>
2016-05-25 14:38:49 -0400

    added website service find website for user

---
+++ public/assignment/services/website.service.client.js
@@ -0,0 +1,31 @@
+(function(){
+    angular.module("WebAppMaker")
+        .factory("WebsiteService", WebsiteService);
+
+    var websites = [
+        { "_id": "123", "name": "Facebook",    "developerId": "456" },
+        { "_id": "234", "name": "Tweeter",     "developerId": "456" },
+        { "_id": "456", "name": "Gizmodo",     "developerId": "456" },
+        { "_id": "567", "name": "Tic Tac Toe", "developerId": "123" },
+        { "_id": "678", "name": "Checkers",    "developerId": "123" },
+        { "_id": "789", "name": "Chess",       "developerId": "234" }
+    ];
+
+    function WebsiteService() {
+
+        var api = {
+            findWebsitesForUser: findWebsitesForUser
+        };
+        return api;
+
+        function findWebsitesForUser(userId) {
+            var result = [];
+            for(var i in websites) {
+                if(websites[i].developerId === userId) {
+                    result.push(websites[i]);
+                }
+            }
+            return result;
+        }
+    }
+})();
